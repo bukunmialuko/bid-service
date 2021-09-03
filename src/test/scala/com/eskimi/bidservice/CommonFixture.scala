4a87aac034840d639e93615d34a948399a045928
bukunmialuko <oluwabukunmi.aluko@gmail.com>
2021-09-03 09:59:37 +0100

    Added test for the actor

---
+++ src/test/scala/com/eskimi/bidservice/CommonFixture.scala
@@ -0,0 +1,49 @@
+package com.eskimi.bidservice
+
+import com.eskimi.bidservice.model._
+import com.eskimi.bidservice.service.CampaignService
+
+object CommonFixture {
+  val bidFloor = 3.12123
+  val impression = Impression(
+    id = "1",
+    wmin = Some(50),
+    wmax = Some(300),
+    w = Some(300),
+    hmin = Some(100),
+    hmax = Some(300),
+    h = Some(250),
+    bidFloor = Some(bidFloor)
+  )
+  val site = Site(
+    id = "0006a522ce0f4bbbbaa6b3c38cafaa0f",
+    domain = "fake.tld"
+  )
+  val geo = Geo(
+    country = Some("LT")
+  )
+  val bidUser = User(
+    id = "USARIO1",
+    geo = Some(geo)
+  )
+  val device = Device(
+    id = "440579f4b408831516ebd02f6e1c31b4",
+    geo = Some(geo)
+  )
+  val requestId = "SGu1Jpq1IO"
+  val bidRequest = BidRequest(
+    id = requestId,
+    imp = Some(List(impression)),
+    site = site,
+    user = Some(bidUser),
+    device = Some(device)
+  )
+  val banner = Banner(
+    id = 1,
+    src = "https://business.eskimi.com/wp-content/uploads/2020/06/openGraph.jpeg",
+    width = 300,
+    height = 250
+  )
+  val campaign = CampaignService.activeCampaigns(2)
+
+}
---
+++ src/test/scala/com/eskimi/bidservice/actor/BidActorSpec.scala
@@ -0,0 +1,48 @@
+package com.eskimi.bidservice.actor
+
+import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
+import com.eskimi.bidservice.actor
+import BidActor.{BidRequestMessage, BidResponseMessage}
+import com.eskimi.bidservice.model._
+import com.eskimi.bidservice.service.{BidResult, BidService}
+import org.mockito.MockitoSugar
+import org.scalatest.wordspec.AnyWordSpecLike
+import com.eskimi.bidservice.CommonFixture._
+
+class BidActorSpec extends ScalaTestWithActorTestKit with AnyWordSpecLike  with MockitoSugar {
+
+  private val bidServiceMock = mock[BidService]
+
+  "A BidActor" must {
+    "reply with bid response in case of successful bid" in {
+      val adId = "1"
+      val bidResponse = BidResponseMessage(
+        Some(BidResponse(
+          id = "response1",
+          bidRequestId = requestId,
+          price = bidFloor,
+          adid = Some(adId),
+          banner = Some(banner)
+        ))
+      )
+      val replyProbe = createTestProbe[BidResponseMessage]()
+      val underTest = spawn(BidActor(bidServiceMock))
+      when(bidServiceMock.processBid(bidRequest)).thenReturn(
+        Some(BidResult(adId, bidFloor, banner))
+      )
+      underTest ! BidRequestMessage(bidRequest, replyProbe.ref)
+      replyProbe.expectMessage(bidResponse)
+    }
+
+    "reply with empty response if not bid" in {
+      val bidResponse = BidResponseMessage(None)
+      val replyProbe = createTestProbe[BidResponseMessage]()
+      val underTest = spawn(actor.BidActor(bidServiceMock))
+      when(bidServiceMock.processBid(bidRequest)).thenReturn(None)
+      underTest ! BidRequestMessage(bidRequest, replyProbe.ref)
+      replyProbe.expectMessage(bidResponse)
+    }
+
+  }
+
+}
